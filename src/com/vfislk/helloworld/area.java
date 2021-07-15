254b46c1789fceaf25a7162e16c7c93b5581ae6b
i722333 <i722333@WXVDSKFPX23263.dsglobal.org>
2021-07-15 11:05:29 +0530

    changes

---
+++ src/com/vfislk/helloworld/area.java
@@ -0,0 +1,13 @@
+package com.vfislk.helloworld;
+
+public class area {
+	public static double areatriangle(int b, int h){
+		return 0.5*b*h;
+	}
+public static double areaCircle(int r){
+		
+		double area = 3.1428*r*r;
+		return area;
+	}
+
+}
--- src/com/vfislk/helloworld/areaofCircle.java
+++ src/com/vfislk/helloworld/areaofCircle.java
@@ -1,21 +1,15 @@
 package com.vfislk.helloworld;
 
 public class areaofCircle {
-	public static double areaCircle(int r){
-		
-		double area = 3.1428*r*r;
-		return area;
-	}
+	
 
-	public static double areatriangle(int b, int h){
-		return 0.5*b*h;
-	}
+	
 	public static void main(String[] args) {
 		int r =10;
-		double result = areaofCircle.areaCircle(r);
+		double result = area.areaCircle(r);
 		System.out.println(result);
 		int b=25,h=1;
-		double res = areaofCircle.areatriangle(b, h);
+		double res = area.areatriangle(b, h);
 		System.out.println(res);
 	}
 
